668449b3f8df482b717dcefbe8f87fb338fca439
xqq <1126048156@qq.com>
2018-03-25 10:51:59 +0800

    smtp

---
+++ email/smtp.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="smtp.aspx.cs" Inherits="email.smtp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            收件人地址：<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            邮件主题：<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            邮件内容：<asp:TextBox ID="TextBox3" runat="server" TextMode="MultiLine"></asp:TextBox>
+        </p>
+        <p>
+            抄送：<asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        </p>
+        添加附件：<asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="发送邮件" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="重新填写" />
+    </form>
+</body>
+</html>
---
+++ email/smtp.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+namespace email
+{
+    public partial class smtp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {         
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MailMessage mymail=new MailMessage();//声明一个mail对象
+            mymail.From=new MailAddress("xxxx@163.com","your name");//发件人地址
+            mymail.To.Add(new MailAddress(TextBox1.Text));//收件人地址
+            mymail.Subject=TextBox2.Text;//邮件主题
+            mymail.Body=TextBox3.Text;//发送邮件的内容
+          //  string filename = Path.GetFileName(FileUpload1.FileName);//获得文件名和后缀
+        
+            //FileUpload1.SaveAs(Server.MapPath("~/") + filename)//保存文件到本程序的文件夹下
+            Attachment myfiles = new Attachment(FileUpload1.PostedFile.InputStream, FileUpload1.PostedFile.FileName);//把上传的文件指向的文件流给附件对象
+            mymail.Attachments.Add(myfiles);//发送邮件附件使用Attachments
+            mymail.CC.Add(new MailAddress(TextBox4.Text));//抄送邮件用CC
+
+            SmtpClient myclient=new SmtpClient();//创建一个邮件服务器
+            myclient.Host="smtp.163.com";//使用的163的SMTP服务器
+            myclient.Port=25;//smtp服务器端口
+            myclient.Credentials=new NetworkCredential("xxxxx@163.com", "xxxxx");//网易的邮箱及密码
+            myclient.Send(mymail);
+            Response.Write("成功！");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+        }
+      }
+    }
